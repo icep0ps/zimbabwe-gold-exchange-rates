0a2d5c710f9a41fa1d22f077a8a654b5eaa2f576
icep0ps <tapsmuko@gmail.com>
2025-07-26 21:49:40 +0200

    feat: batch rates support

--- backend/src/db/seeder.ts
+++ backend/src/db/seeder.ts
@@ -1,42 +1,54 @@
+import { and, desc, eq } from "drizzle-orm";
 import type { PgDatabase } from "drizzle-orm/pg-core";
-import type { ExtractedRates } from "../rates/extractRates.js";
-import { type NewCurrency, currencies } from "./schema.js";
+import type { SuccessExtractionResponse } from "../scripts/get-latest-rate-script.js";
+import { getPreviousDateString, scriptLogger } from "../utils.js";
+import { type NewRate, rates as ratesTable } from "./schema.js";
 
 export async function seedRatesToDatabase(
   db: PgDatabase<any, any, any>,
-  rates: ExtractedRates,
-  createdAt: string,
+  data: Omit<SuccessExtractionResponse, "success">[],
 ): Promise<void> {
-  console.log("Attempting to seed extracted rates into the database...");
+  scriptLogger.info("Starting database seeding for extracted rates");
 
-  const dataToInsert: NewCurrency[] = [];
+  const dataToInsert = await addIdsOnRates(data, db);
 
-  for (const currency in rates) {
-    if (Object.prototype.hasOwnProperty.call(rates, currency)) {
-      const rateValues = rates[currency];
-
-      const newRate: NewCurrency = {
-        currency: currency,
-        bid: rateValues.bid.toString(),
-        ask: rateValues.ask.toString(),
-        mid_rate: rateValues.mid_rate.toString(),
-        bid_rate_zwg: rateValues.bid_zwl.toString(),
-        ask_rate_zwg: rateValues.ask_zwl.toString(),
-        mid_rate_zwg: rateValues.mid_zwl.toString(),
-        created_at: createdAt,
-      };
-      dataToInsert.push(newRate);
-    }
-  }
-
-  if (dataToInsert.length === 0) {
-    console.log("No rates to seed. Skipping database insertion.");
+  if (!dataToInsert || dataToInsert.length === 0) {
+    scriptLogger.error("No rates to seed. Skipping database insertion.");
     return;
   }
 
   try {
-    await db.insert(currencies).values(dataToInsert);
-    console.log(
+    const oldestRate = dataToInsert[0];
+    const dateBeforeOldestRate = getPreviousDateString(
+      oldestRate.created_at as string,
+    );
+
+    const [oldestRatesPreviousRate] = await db
+      .select()
+      .from(ratesTable)
+      .where(
+        and(
+          eq(ratesTable.currency, oldestRate.currency),
+          eq(ratesTable.created_at, dateBeforeOldestRate),
+        ),
+      )
+      .limit(1);
+
+    const dataToInsertWithPrevousRates: NewRate[] = [];
+
+    scriptLogger.info("Adding previous rates");
+    dataToInsert.reduce((previousRate, currentRate) => {
+      const rateWithId = {
+        ...currentRate,
+        previous_rate: previousRate?.id ?? null,
+      };
+
+      dataToInsertWithPrevousRates.push(rateWithId);
+      return rateWithId;
+    }, oldestRatesPreviousRate);
+
+    await db.insert(ratesTable).values(dataToInsertWithPrevousRates);
+    scriptLogger.info(
       `Successfully seeded ${dataToInsert.length} currency rates into the database.`,
     );
   } catch (error: unknown) {
@@ -45,5 +57,51 @@ export async function seedRatesToDatabase(
       errorMessage = error.message;
     }
     throw new Error(`Error seeding rates to database: ${errorMessage}`);
+  }
+}
+
+async function addIdsOnRates(
+  rates: Omit<SuccessExtractionResponse, "success">[],
+  db: PgDatabase<any, any, any>,
+) {
+  scriptLogger.info("Adding ID's to extracted rates");
+  const dataToInsert: Omit<NewRate, "previous_rate">[] = [];
+  try {
+    const [lastestRate] = await db
+      .select()
+      .from(ratesTable)
+      .orderBy(desc(ratesTable.id))
+      .limit(1);
+
+    let lastestRateId = lastestRate?.id ?? 1;
+
+    rates.forEach((rate) => {
+      const rates = rate.data;
+
+      for (const currency in rates) {
+        if (Object.prototype.hasOwnProperty.call(rates, currency)) {
+          const rateValues = rates[currency];
+
+          const newRate = {
+            id: lastestRateId++,
+            currency: currency,
+            bid: rateValues.bid.toString(),
+            ask: rateValues.ask.toString(),
+            mid_rate: rateValues.mid_rate.toString(),
+            bid_rate_zwg: rateValues.bid_zwl.toString(),
+            ask_rate_zwg: rateValues.ask_zwl.toString(),
+            mid_rate_zwg: rateValues.mid_zwl.toString(),
+            created_at: rateValues.created_at.toString(),
+          };
+
+          dataToInsert.push(newRate);
+        }
+      }
+    });
+
+    scriptLogger.info("Successfully added ID's");
+    return dataToInsert;
+  } catch (error) {
+    scriptLogger.error("something went wrong while trying to add id's");
   }
 }
