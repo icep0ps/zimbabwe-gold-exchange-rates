59d16a4b47d9e8318689d8df7c8fd0ad529eb120
icep0ps <tapsmuko@gmail.com>
2026-03-18 01:53:49 +0200

    fix: simplify API exports and align vercel routing with flat architecture

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,8 @@
 import { handle } from "hono/vercel";
 import { app } from "../src/index";
 
-export const GET = handle(app);
-export const POST = handle(app);
-export const PUT = handle(app);
-export const DELETE = handle(app);
-export const PATCH = handle(app);
-export const OPTIONS = handle(app);
+export const config = {
+  runtime: "nodejs20.x",
+};
 
 export default handle(app);
